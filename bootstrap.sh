c3bc1f20918099e176e2b8d62e85ba03eeb315bc
Renan A. Marks <contato@renanmarks.net>
2023-05-31 14:24:38 -0400

    Update the requirements to make Nuskell work.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@
 sudo apt-get update
 sudo apt-get install -y  python python2.7 python2.7-dev python-setuptools python-pip python-virtualenv python-pandas python-seaborn python-sympy python-networkx python-future python-matplotlib python-xlrd cmake g++ #python3-pip python3-virtualenv 
 sudo apt-get install -y python3.8 python3.8-dev python3-setuptools python3-pip python3-virtualenv python3-networkx python3-future python3-xlrd
+sudo apt-get purge -y python3-numpy
 
 # Make dir to hold all the software
 mkdir winfree
@@ -13,7 +14,7 @@ cd winfree
 git clone https://github.com/DNA-and-Natural-Algorithms-Group/multistrand.git
 git clone https://github.com/DNA-and-Natural-Algorithms-Group/KinDA.git
 git clone https://github.com/DNA-and-Natural-Algorithms-Group/peppercornenumerator.git
-git clone https://github.com/DNA-and-Natural-Algorithms-Group/nuskell.git
+git clone --branch v0.8  https://github.com/DNA-and-Natural-Algorithms-Group/nuskell.git
 
 # Get the Nupack software
 cp /vagrant/nupack-3.2.2.tar.gz .
@@ -62,5 +63,5 @@ sudo python setup.py install
 # Build and install Nuskell
 
 cd ../nuskell
+pip3 install numpy==1.15.4 versioneer pandas matplotlib scipy seaborn
 sudo python3.8 setup.py install
-pip3 install numpy matplotlib scipy pandas seaborn
